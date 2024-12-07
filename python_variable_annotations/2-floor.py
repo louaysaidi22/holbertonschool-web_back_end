03bd6126d2696565f86e49c0998c153546cfecda
louaysaidi22 <louaysaidi18@gmail.com>
2024-12-07 14:16:30 +0100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"Basic annotations - floor"
+
+
+def floor(n: float) -> int:
+    """add is a type-annotated function that takes one argument:
+        - 2 float n
+       and returns:
+        the floor of the float"""
+    return int(n)
