e99e2775143b60db4d38d8a2f744ed662a8ee5d5
louaysaidi22 <louaysaidi18@gmail.com>
2024-12-07 14:10:02 +0100

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"Basic annotations - concat"
+
+
+def concat(str1: str, str2: str) -> str:
+    """add is a type-annotated function that takes two arguments:
+        -string str1
+        -string str2
+       and returns:
+        a concatenated string"""
+    return str1 + str2
