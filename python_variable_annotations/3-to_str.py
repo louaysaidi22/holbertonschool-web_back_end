d0ddc0db1b9f1b9ea7744af266c2a335321c0863
louaysaidi22 <louaysaidi18@gmail.com>
2024-12-07 14:21:42 +0100

    task ~

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"Basic annotations - to string"
+
+
+def to_str(n: float) -> str:
+    """add is a type-annotated function that takes one argument:
+        - float n
+       and returns:
+        - the string representation of the float"""
+    return str(n)
