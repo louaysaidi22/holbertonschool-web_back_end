ee758807d0b1e06c2d927a71d74bc54670064a64
louaysaidi22 <louaysaidi18@gmail.com>
2024-12-07 14:31:38 +0100

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"Define and annotate multiple variables with the specified values"
+
+
+" an integer with a value of 1:"
+a: int = 1
+"a float with a value of 3.14:"
+pi: float = 3.14
+"a boolean with a value of True:"
+i_understand_annotations: bool = True
+"a string with a value of “Holberton”:"
+school: str = "Holberton"
